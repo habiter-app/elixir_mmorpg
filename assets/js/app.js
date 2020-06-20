7561e204c9290ee9442460b37d990c168ae0197a
Alessandro Solbiati <lessandro@fb.com>
2020-06-20 22:42:36 +0100

    three.js example with elixir webserver

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,8 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+import Game from "./game"
+document.addEventListener("DOMContentLoaded", function(){
+	const game = new Game();
+	window.game = game;//For debugging only
+});
