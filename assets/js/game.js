69e4c6c95bc1ff393544a5c51dff8cff623023c3
Alessandro Solbiati <lessandro@fb.com>
2020-06-22 21:36:42 +0100

    [environment] custom scene, texture broken

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,6 @@ import { FBXLoader } from "./vendor/FBXLoader"
 class Game{
     // preload game assets
     constructor(){
-
 	  const game = this;
 	  this.player = { };
 	  this.modes = Object.freeze({
@@ -44,7 +43,22 @@ class Game{
 		// scene setup, light camera and background.
         this.scene = new THREE.Scene();
 	    this.scene.background = new THREE.Color( 0xa0a0a0 );
-	    this.scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
+
+        let light = new THREE.HemisphereLight( 0xffffff, 0x444444, 1 );
+        light.position.set( 0, 500, 0 );
+        this.scene.add( light );
+ 
+        light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 0, 200, 100 );
+        light.castShadow = true;
+        light.shadow.mapSize.width = 2048;
+        light.shadow.mapSize.height = 2048;
+        light.shadow.camera.top = 3000;
+        light.shadow.camera.bottom = -3000;
+        light.shadow.camera.left = -3000;
+        light.shadow.camera.right = 3000;
+        light.shadow.camera.far = 3000;
+        this.scene.add( light );
 
         // ground
         var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry    ( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999,     depthWrite: false } ) );
@@ -53,15 +67,13 @@ class Game{
         mesh.receiveShadow = true;
         this.scene.add( mesh );
 
-        const light = new THREE.DirectionalLight( 0xffffff );
-		light.position.set( 0, 20, 10 );
-        const ambient = new THREE.AmbientLight( 0x707070 ); // soft white light
+	    var light_2 = new THREE.PointLight( 0x000000, 1000, 100 );
+	    light_2.position.set( 50, 50, 50 );
+	    this.scene.add( light_2 );
 
 		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 	    this.camera.position.set(112, 100, 200);
 	    this.camera.quaternion.set(0.07133122876303646, -0.17495722675648318,     -0.006135162916936811, -0.9819695435118246);
-        this.scene.add( light );
-        this.scene.add( ambient );
 
 		this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize( window.innerWidth, window.innerHeight );
@@ -122,27 +134,27 @@ class Game{
 		if (event.code === "KeyD") game.player.move.direction = 0;
 	  })
 
-	  game.createDummyEnvironment();
+	  game.loadEnvironment(loader);
 	}
 
-    createDummyEnvironment(){
-        const env = new THREE.Group();
-        env.name = "Environment";
-        this.scene.add(env);
-
-        const geometry = new THREE.BoxBufferGeometry( 150, 150, 150 );
-        const material = new THREE.MeshBasicMaterial( { color: 0x285064 } );
-
-        for(let x=-1000; x<1000; x+=300){
-            for(let z=-1000; z<1000; z+=300){
-                const block = new THREE.Mesh(geometry, material);
-                block.position.set(x, 75, z);
-                env.add(block);
-            }
-        }
-
-        this.environmentProxy = env;
-    }
+  
+	loadEnvironment(loader){
+	  const game = this;
+	  loader.load( `${this.assetsPath}game_environment.fbx`, function(object){
+		game.scene.add(object);
+		object.receiveShadow = true;
+		object.name = "Environment"
+		object.scale.set(0.6, 0.6, 0.6);
+		object.rotateY( Math.PI / 1.5);
+		object.position.y = -20
+        object.traverse( function ( child ) {
+            if ( child.isMesh ) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+			}});
+		 game.environmentProxy = object;
+	  });
+	}
 
     set action(name){
 	  const animation = this.player[name]
